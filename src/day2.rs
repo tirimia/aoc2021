6403776dffed077a8d864d7eafb3add5595414a9
Theodor-Alexandru Irimia <Theodor-Alexandru.Irimia@trivago.com>
2021-12-01 11:19:27 +0100

    Initial commit + day 1

---
+++ src/day2.rs
@@ -0,0 +1,10 @@
+use crate::read_lines;
+
+pub fn day_2() -> String {
+    let _input: Vec<usize> = read_lines("assets/day2.input")
+        .iter()
+        .map(|l| l.parse::<usize>().unwrap())
+        .collect();
+    // format!("Part 1: {}\nPart 2: {}", day1a(input.clone()), day1b(input))
+    "TODO".to_string()
+}
