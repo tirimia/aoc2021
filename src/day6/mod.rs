d65bb5134ffdbcce1896d55941dc46c4d5b3085e
Theodor-Alexandru Irimia <Theodor-Alexandru.Irimia@trivago.com>
2021-12-07 14:38:34 +0100

    Refactor to use an array

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,22 +1,59 @@
 mod verify;
 use std::collections::HashMap;
 
+const FISH_TIMERS: usize = 9;
+
 fn day6_base(state: Vec<usize>, days: usize) -> usize {
     (0..days)
         .into_iter()
-        .fold(HashMap::init(state), |s, _| s.iterate())
-        .total()
+        .fold(init(state), |s, _| s.iterate())
+        .iter()
+        .sum()
 }
 
 pub fn day6a(state: Vec<usize>) -> usize { day6_base(state, 80) }
 
 pub fn day6b(state: Vec<usize>) -> usize { day6_base(state, 256) }
 
+fn init(state: Vec<usize>) -> [usize; FISH_TIMERS] {
+    let mut list: [usize; FISH_TIMERS] = [0; FISH_TIMERS];
+    for fish in state {
+        list[fish] += 1;
+    }
+    list
+}
+
 trait LanternfishCounter {
     fn init(state: Vec<usize>) -> Self;
     fn iterate(&self) -> Self;
-    fn total(self) -> usize;
 }
+
+impl LanternfishCounter for [usize; FISH_TIMERS] {
+    fn init(state: Vec<usize>) -> Self { init(state) }
+
+    fn iterate(&self) -> Self {
+        let mut list: [usize; FISH_TIMERS] = [0; FISH_TIMERS];
+        list[8] = self[0];
+        list[6] = self[0];
+        for i in 1..FISH_TIMERS {
+            list[i - 1] += self[i];
+        }
+        list
+    }
+}
+
+/// HashMap implementation
+fn day6_base_hash(state: Vec<usize>, days: usize) -> usize {
+    (0..days)
+        .into_iter()
+        .fold(HashMap::init(state), |s, _| s.iterate())
+        .values()
+        .sum()
+}
+
+pub fn day6a_hash(state: Vec<usize>) -> usize { day6_base_hash(state, 80) }
+
+pub fn day6b_hash(state: Vec<usize>) -> usize { day6_base_hash(state, 256) }
 
 impl LanternfishCounter for HashMap<usize, usize> {
     fn init(state: Vec<usize>) -> Self {
@@ -27,8 +64,6 @@ impl LanternfishCounter for HashMap<usize, usize> {
                 acc
             })
     }
-
-    fn total(self) -> usize { self.values().sum() }
 
     fn iterate(&self) -> Self {
         let mut new = HashMap::with_capacity(9);
